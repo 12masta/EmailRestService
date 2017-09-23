22f8d06c9b02226c7fd5d0f51317b5afa730f33c
12masta <stanek.marcinp@gmail.com>
2017-09-23 22:44:55 +0200

    Greeting controller init

---
+++ emailrestservice/src/main/java/com/ms/emailrestservice/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package com.ms.emailrestservice.controllers;
+
+import com.ms.emailrestservice.models.GreetingModel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public GreetingModel greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new GreetingModel(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
