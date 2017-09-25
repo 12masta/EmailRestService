dd89f1cc15accf7574b107f2b3d4e1a77f7de143
12masta <stanek.marcinp@gmail.com>
2017-09-25 21:13:46 +0200

    GreetingController added request method

--- service/src/main/java/com/ms/emailrestservice/controllers/GreetingController.java
+++ service/src/main/java/com/ms/emailrestservice/controllers/GreetingController.java
@@ -1,11 +1,12 @@
 package com.ms.emailrestservice.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import com.ms.emailrestservice.models.GreetingModel;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
@@ -13,8 +14,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public GreetingModel greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public GreetingModel greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new GreetingModel(counter.incrementAndGet(),
                 String.format(template, name));
     }
