0e6ccf0b728104e62c0e7c867eaa7ab915e68522
12masta <stanek.marcinp@gmail.com>
2017-10-03 01:12:10 +0200

    Email Service integration spec

--- service/src/test/groovy/com/ms/emailrestservice/test/integration/EmailServiceIntegrationSpec.groovy
+++ service/src/test/groovy/com/ms/emailrestservice/test/integration/EmailServiceIntegrationSpec.groovy
@@ -1,9 +1,11 @@
 package com.ms.emailrestservice.test.integration
 
 import com.ms.emailrestservice.Application
+import com.ms.emailrestservice.controllers.GreetingController
 import com.ms.emailrestservice.test.EmailServiceSpecification
 import okhttp3.Request
 import okhttp3.Response
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 
@@ -11,10 +13,14 @@ import org.springframework.test.context.ContextConfiguration
 @SpringBootTest(classes = Application.class)
 class EmailServiceIntegrationSpec extends EmailServiceSpecification {
 
+    @Autowired
+    GreetingController controller
+
     def "Call sendSimpleMessage method in EmailService with valid arguments"() {
         given: "request has been build"
         def request = new Request.Builder()
                 .url("http://localhost:8080/greeting")
+                .get()
                 .build()
 
         when: "request has been executed"
