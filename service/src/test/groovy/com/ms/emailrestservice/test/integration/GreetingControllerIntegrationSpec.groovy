eb4c8379b692a8defb19b2fa8b5267eac3818b04
12masta <stanek.marcinp@gmail.com>
2017-10-13 00:26:05 +0200

    GreetingController integration test added

--- service/src/test/groovy/com/ms/emailrestservice/test/integration/GreetingControllerIntegrationSpec.groovy
+++ service/src/test/groovy/com/ms/emailrestservice/test/integration/GreetingControllerIntegrationSpec.groovy
@@ -1,35 +1,27 @@
 package com.ms.emailrestservice.test.integration
 
 import com.ms.emailrestservice.Application
-import com.ms.emailrestservice.controllers.GreetingController
 import com.ms.emailrestservice.test.EmailServiceSpecification
 import okhttp3.Request
-import okhttp3.Response
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 
-@ContextConfiguration
-@SpringBootTest(classes = Application.class)
-class EmailServiceIntegrationSpec extends EmailServiceSpecification {
-
-    @Autowired
-    GreetingController controller
+@ContextConfiguration(classes = Application.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class GreetingControllerIntegrationSpec extends EmailServiceSpecification {
 
     def "Call sendSimpleMessage method in EmailService with valid arguments"() {
         given: "request has been build"
         def request = new Request.Builder()
                 .url("http://localhost:8080/greeting")
                 .get()
+                .header("Content-Type", "application/json")
                 .build()
 
         when: "request has been executed"
-        Response response = client.newCall(request).execute()
+        def response = client.newCall(request).execute()
 
         then: "no exception was throwed"
-        response.body().toString().contains("Hello")
-        where:
-        to                         | subject             | text
-        "stanek.marcinp@gmail.com" | "test_subject_mail" | "test text"
+        response.body().string().contains("Hello, World")
     }
 }
